049c3fcfea043a194048851bfd4232f1fea42a7f
duartej <jordid@post.tau.ac.il>
2014-11-04 09:34:59 +0200

    First commit PyAnUtils package-container of python utilities for physics analysis

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup, Extension
+
+setup(name='PyAnUtils',
+		version='0.1',
+		description='Python Analysis Utils',
+		author='Jordi Duarte-Campderros',
+		author_email='Jordi.Duarte.Campderros@cern.ch',
+		url='https://github.com/',
+		packages = ['PyAnUtils' ],
+		package_dir={'PyAnUtils':''},
+		#scripts=[],
+		)
+																				)
