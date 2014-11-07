9d6d53c125d5c10cc1a2cdc43d945a1fa63cfe01
duartej <jordid@post.tau.ac.il>
2014-11-07 12:21:52 +0200

    added 2d version of graphtohist

--- python/pyanfunctions.py
+++ python/pyanfunctions.py
@@ -28,7 +28,7 @@ def graphtohist(graph,binning=1000):
 	xmin = graph.GetXaxis().GetBinLowEdge(graph.GetXaxis().GetFirst())
 	xmax = graph.GetXaxis().GetBinUpEdge(graph.GetXaxis().GetLast())
 	h = TH1F(graph.GetName()+'_histo','',binning,xmin,xmax)
-	xbins = [ h.GetXaxis().GetBinLowEdge(i) for i in xrange(1,h.GetNbinsX()+2) ]
+	#xbins = [ h.GetXaxis().GetBinLowEdge(i) for i in xrange(1,h.GetNbinsX()+2) ]
 	yval = Double(0.0)
 	xval = Double(0.0)
 	# dict to count how many entries are pushed in the same bin, in order to
@@ -37,6 +37,51 @@ def graphtohist(graph,binning=1000):
 	for i in xrange(graph.GetN()):
 		point = graph.GetPoint(i,xval,yval)
 		_hbin = h.Fill(xval,yval)
+		try:
+			hdict[_hbin] +=1 
+		except KeyError:
+			hdict[_hbin] = 1
+	# Let's average the bins with more than one entry
+	for b,nentries in hdict.iteritems():
+		valaux = h.GetBinContent(b)
+		h.SetBinContent(b,valaux/nentries)
+	
+	return h
+
+def graphtohist2D(graph,binning=1000):
+	""".. function:: graphtohist2D(graph,binning=1000) -> ROOT.TH2F
+
+	2-dimensional version of the graphtohist2D function
+
+	:graph: The graph to be converted
+	:graph type: ROOT.TGraph2D	
+	:binning: Number of desired bins
+	:binning type: float
+
+	:return: A histogram based in the TGraph2D
+	:rtype: ROOT.TH2F
+
+	FIXME: Possible can be absorved by the graphtohist function
+	just minor addition are needed
+	"""
+	from ROOT import Double,TH2F
+	# Extract limits to build the histo
+	xmin = graph.GetXmin()
+	xmax = graph.GetXmax()
+	ymin = graph.GetYmin()
+	ymax = graph.GetYmax()
+	h = TH2F(graph.GetName()+'_histo','',binning,xmin,xmax,binning,ymin,ymax)
+	#xbins = [ h.GetXaxis().GetBinLowEdge(i) for i in xrange(1,h.GetNbinsX()+2) ]
+	#ybins = [ h.GetYaxis().GetBinLowEdge(i) for i in xrange(1,h.GetNbinsY()+2) ]
+	yval = Double(0.0)
+	xval = Double(0.0)
+	zval = Double(0.0)
+	# dict to count how many entries are pushed in the same bin, in order to
+	# make an average later
+	hdict = {}
+	for i in xrange(graph.GetN()):
+		point = graph.GetPoint(i,xval,yval,zval)
+		_hbin = h.Fill(xval,yval,zval)
 		try:
 			hdict[_hbin] +=1 
 		except KeyError:
