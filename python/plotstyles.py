1443a701d25fb940014d94a904489907f3291f8f
duartej <jordid@post.tau.ac.il>
2014-11-07 12:21:11 +0200

    Added new style. Added setpalette function where several palettes are pre-defined (dark body radiation, deep sea, rainbow, gray, ...)

--- python/plotstyles.py
+++ python/plotstyles.py
@@ -37,7 +37,7 @@ def squaredStyle():
 	squaredStyle.SetPadBorderMode  (   0)
 	squaredStyle.SetPadBorderSize  (  10)
 	squaredStyle.SetPadColor       (   0)
-	squaredStyle.SetPadBottomMargin(0.20)
+	squaredStyle.SetPadBottomMargin(0.18)
 	squaredStyle.SetPadTopMargin   (0.08)
 	squaredStyle.SetPadLeftMargin  (0.18)
 	squaredStyle.SetPadRightMargin (0.05)
@@ -184,3 +184,140 @@ def atlasStyle():
 	#gStyle.SetPadTickY(1)
 	return atlasStyle
 
+def njStyle():
+	""".. function:: njStyle() -> ROOT.gStyle
+	
+	Return a ROOT.gStyle based in Castello-Mor and
+	Duarte-Campderros mixed styles.
+	"""	
+	import ROOT
+	ROOT.GloStyle = ROOT.gStyle
+	#jl TStyle
+	njStyle = ROOT.TStyle('jlStyle', "N-J Style");
+
+	#set the background color to white
+	njStyle.SetFillColor(10);
+	njStyle.SetFrameFillColor(10);
+	njStyle.SetCanvasColor(10);
+	njStyle.SetPadColor(10);
+	njStyle.SetTitleFillColor(0);
+	njStyle.SetStatColor(10);
+
+	#dont put a colored frame around the plots
+	njStyle.SetFrameBorderMode(0);
+	njStyle.SetCanvasBorderMode(0);
+	njStyle.SetPadBorderMode(0);
+	njStyle.SetLegendBorderSize(0);
+
+	#use the primary color palette
+	njStyle.SetPalette(1);
+	
+	#set the default line color for a histogram to be black
+	njStyle.SetHistLineColor(ROOT.kBlack);
+	
+	#set the default line color for a fit function to be red
+	njStyle.SetFuncColor(ROOT.kRed);
+	
+	#make the axis labels black
+	njStyle.SetLabelColor(ROOT.kBlack,"xyz");
+	
+	#set the default title color to be black
+	njStyle.SetTitleColor(ROOT.kBlack);
+	 
+	#set the margins
+	njStyle.SetPadBottomMargin(0.18);
+	njStyle.SetPadTopMargin(0.08);
+	njStyle.SetPadRightMargin(0.08);
+	njStyle.SetPadLeftMargin(0.17);
+
+	#set axis label and title text sizes
+	njStyle.SetLabelFont(42,"xyz");
+	njStyle.SetLabelSize(0.05,"xyz");
+	njStyle.SetLabelOffset(0.015,"xyz");
+	njStyle.SetTitleFont(42,"xyz");
+	njStyle.SetTitleSize(0.05,"xyz");
+	njStyle.SetTitleOffset(1.2,"yz");
+	njStyle.SetTitleOffset(1.5,"x");
+	njStyle.SetStatFont(42);
+	njStyle.SetStatFontSize(0.09);
+	njStyle.SetTitleBorderSize(0);
+	njStyle.SetStatBorderSize(0);
+	njStyle.SetTextFont(42);
+	
+	#set line widths
+	njStyle.SetFrameLineWidth(2);
+	njStyle.SetFuncWidth(2);
+	njStyle.SetHistLineWidth(2);
+	
+	#set the number of divisions to show
+	njStyle.SetNdivisions(506, "xy");
+	
+	#turn off xy grids
+	njStyle.SetPadGridX(0);
+	njStyle.SetPadGridY(0);
+	
+	#set the tick mark style
+	njStyle.SetPadTickX(1);
+	njStyle.SetPadTickY(1);
+	
+	#turn off stats
+	njStyle.SetOptStat(0);
+	njStyle.SetOptFit(0);
+	
+	#marker settings
+	njStyle.SetMarkerStyle(20);
+	njStyle.SetMarkerSize(0.7);
+	njStyle.SetLineWidth(2); 
+
+	#done
+	#njStyle.cd();
+	#ROOT.gROOT.ForceStyle();
+	#ROOT.gStyle.ls();
+	return njStyle
+
+def setpalette(name="rainbow", ncontours=999):
+	""".. function::setpalette()
+	
+	Set a color palette from a given RGB list
+	stops, red, green and blue should all be lists 
+	of the same length 
+	see set_decent_colors for an example"""
+	from ROOT import TColor,gStyle
+	from array import array
+	
+	if name == "gray" or name == "grayscale":
+		stops = [0.00, 0.34, 0.61, 0.84, 1.00]
+		red   = [1.00, 0.84, 0.61, 0.34, 0.00]
+		green = [1.00, 0.84, 0.61, 0.34, 0.00]
+		blue  = [1.00, 0.84, 0.61, 0.34, 0.00]
+	elif name == 'darkbody':
+		stops = [0.00, 0.25, 0.50, 0.75, 1.00]
+		red   = [0.00, 0.50, 1.00, 1.00, 1.00]
+		green = [0.00, 0.00, 0.55, 1.00, 1.00]
+		blue  = [0.00, 0.00, 0.00, 0.00, 1.00]
+	elif name == 'inv_darkbody':
+		stops = [0.00, 0.25, 0.50, 0.75, 1.00] 
+		red   = [1.00, 1.00, 1.00, 0.50, 0.00]
+		green = [1.00, 1.00, 0.55, 0.00, 0.00]
+		blue  = [1.00, 0.00, 0.00, 0.00, 0.00]
+	elif name == 'deepsea':
+		stops = [0.00, 0.34, 0.61, 0.84, 1.00]  
+		red   = [0.00, 0.09, 0.18, 0.09, 0.00]
+		green = [0.01, 0.02, 0.39, 0.68, 0.97] 
+		blue  = [0.17, 0.39, 0.62, 0.79, 0.97] 
+	else:
+		# default palette, looks cool
+		stops = [0.00, 0.34, 0.61, 0.84, 1.00]
+		red   = [0.00, 0.00, 0.87, 1.00, 0.51]
+		green = [0.00, 0.81, 1.00, 0.20, 0.00]
+		blue  = [0.51, 1.00, 0.12, 0.00, 0.00]
+
+		
+	s = array('d', stops)
+	r = array('d', red)
+	g = array('d', green)
+	b = array('d', blue)
+	
+	npoints = len(s)
+	TColor.CreateGradientColorTable(npoints, s, r, g, b, ncontours)
+	gStyle.SetNumberContours(ncontours)
