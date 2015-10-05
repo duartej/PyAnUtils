428874a0f8ebf73273369673576079c7fa0bc845
Jordi Duarte Campderros <Jordi.Duarte.Campderros@cern.ch>
2015-10-05 16:04:36 +0200

    improved the encapsulation of the pool files access

--- python/retrievetrees.py
+++ python/retrievetrees.py
@@ -15,7 +15,8 @@ from abc import abstractmethod
 class storedtree(object):
     """Abstract class to implement the retrieval of a tree-type (n-tuple) root file.
     The concrete classes should implement the relative methods dependents of the
-    ntuple obtainer (RPVMCInfoTree/RPVMCTruthHist/...)
+    ntuple obtainer (RPVMCInfoTree/RPVMCTruthHist/...). The class is intented to
+    deal with the main plots 
     Virtual methods:
      * gettreechain
      * getmctruthinfo
@@ -34,49 +35,144 @@ class storedtree(object):
         """
         import ROOT
         import os
-        
-        self._rootfiles = rootfiles
+
+        # should be a list
+        if type(rootfiles) is not list:
+            self._rootfiles = [rootfiles]
+        else:
+            self._rootfiles = rootfiles
         
         self._tree = ROOT.TChain(chainname)
         print "TChain '%s' created" % chainname
-        print "Adding %i root files to the tree" % len(self.rootfiles)
+        print "Adding %i root files to the tree" % len(self._rootfiles)
         for i in sorted(self._rootfiles):
             self._tree.AddFile(i)
         
         self._plotsactivated = False
-        self._nentries   = self.tree.GetEntries()
-        self._wise_values = {}
+        self._nentries   = self._tree.GetEntries()
+        self._wise_values= {}
+        self._aliases    = {}
+        self._currententry = -1
         # The self._vars attribute is created in the concrete implementations
 
 
-    def activate_variables(self,varname,alias=None):
-        """The varname variable is activated and will be filled in the entry loop
-        of the tree. If there is no varname, it is going to be used all of them 
-        (content of the _vars datamember). If varname is a tuple of str, a
-        TH2F is going to be created, while if is a str or a list of str, a TH1F
-        foe each variable is created
+    def activate_variable(self,varname,methodtouse=None,**kwd):
+        """The varname variable is encapsulated to be includede in this
+        instance and an attribute with the name `varname` (or `alias` if
+        was used) is settled. If this method is called with the argument
+        `methodtouse` the implicit assumption is that `varname` is
+        representing a class in the original TTree of the file, therefore
+        the `methodtouse` must be a valid method of the `varname` class 
+        (as a string, and without the call '()', see `Examples`). In
+        this case, the encapsulation of the method is also performed and
+        can be accessed through the attribute built from 'varname_methodtouse'
+        Note that the method call is changing in order to use the index of 
+        the i-element on the case of a vector variable.
+
     
         Parameters
         -----------
-        varname: str | tuple(str,str) | list(str)
-            the name of the variable(s) to activate
-        histos:  ROOT.TH1F | ROOT.TH2F | list(ROOT.TH1F)
-            the associated histogram to the variables. It must match the content 
-            of the varname parameter
+        varname: str
+            the name of the variable. It is a ROOT.TBranch (or inheriting class)
+            on the input root file
+        methodtosue: str [Default: None]
+            a valid method of the class which `varname` is an instance
+        alias: str, optional
+            the name to be used as attribute instead of `varname`
+        isvector: bool, optional
+            whether or not `varname` is a std::vector in the input tree
+        methodshort: str, optional
+            a short name to be used as attribute instead of `methodtouse`
+        nocallmethod: bool, optional
+            whether the method `methodtouse` should not be called or yes, i.e.
 
+        Examples
+        --------
+        The root file contains a TTree called "CollectionTree" and a 
+        TBranchElement called 'InDetHighD0Particles':
+        ...
+        <ROOT.TBranchElement object ("InDetHighD0TrackParticles") at 0x4a2b290>
+        ...
+        which is a std::vector of instances of the class "xAOD::TrackParticles".
+        This class has a method "xAOD::TrackParticles::d0()", 
+        "xAOD::TrackParticles::z0()". The following lines shows the 
+        instantiation of the class and the encapsulation of those methods:
+
+        >>> a = xaodtree('filename.root')
+        >>> a.activate_variable('InDetHighD0TrackParticles','d0')
+        >>> a.activate_variable('InDetHighD0TrackParticles','z0')
+
+        To obtain the d0 of the i-track in the j-event:
+        >>> a.getentry(j)
+        >>> a.InDetHighD0TrackParticles_d0(i)
+        
         Raises
         ------
+
+        Notes on implementation
+        -----------------------
+        The main data-member is the '_wise_values' dictionary. Each
+        key of the dictionary is filled with the name of the Tree-branch
+        variable, and the values are 2-tuples composed by the actual
+        accessor and a dictionary with the methods associtated to that
+        variable and the values are the actual accessors of those methods:
+
+        _wise_values = { 'branchvariablename': (ROOT.ClassOfbranchvariablename, 
+                                   { 'method1': method1functor(int index), ...} ) }
         """
-        if not alias:
+        from PyAnUtils.pyanfunctions import ExtraOpt
+        opt = ExtraOpt( [('alias',None), ('isvector',True), \
+                ('methodshort',None), ('nocallmethod',False)] )
+        opt.setkwd(kwd)
+        
+        if not opt.alias:
             aliasname = varname
         else:
-            aliasname = alias
+            aliasname = opt.alias
+            # Not needed below line... to be deprecated this data-member
+            self._aliases[aliasname] = opt.alias
 
-        try:
-            self._wise_value[aliasname] = getattr(self._tree,varname)
-        except AttributeError:
-            raise AttributeError("Not found variable '%s' in the Tree" % varname)
+        methodimpl = methodtouse
+        if opt.methodshort:
+            methodname = opt.methodshort
+        else:
+            methodname = methodtouse
 
+        if opt.isvector:
+            if opt.nocallmethod:
+                extractfunc = lambda k: getattr(self._wise_values[aliasname][0][k],methodimpl)
+            else:
+                extractfunc = lambda k: getattr(self._wise_values[aliasname][0][k],methodimpl)()
+        else:
+            extractfunc = lambda k=None: getattr(self._wise_values[aliasname][0],methodimpl)
+        
+        # First initialization
+        if not self._wise_values.has_key(aliasname):
+            # Note that an exception will be raised by ROOT if the attribute 
+            # doesn't exist in the TTree
+            self._wise_values[aliasname] = (getattr(self._tree,varname),{})
+            # Also create an accessor
+            setattr(self,aliasname,self._wise_values[aliasname][0])
+            # Be careful, only if is a vector make sense
+            try:
+                setattr(self,aliasname+'_size',self._wise_values[aliasname][0].size())
+            except AttributeError:
+                pass
+
+        # Just initialize the variable
+        if not methodtouse:
+            return
+
+        self._wise_values[aliasname][1][methodname] = extractfunc
+        # how to obtain the variable from the tree: methodtouse, check it if
+        # is works
+        if self._currententry == -1:
+            self._tree.GetEntry(0)
+        # Check if the attribute exits, othewise raised exception by ROOT
+        _dummy = self._wise_values[aliasname][1][methodname](0)
+        # Just making  an accessor: nameOfVariable_method
+        setattr(self,aliasname+'_'+methodname,self._wise_values[aliasname][1][methodname])
+        
 
     def activate_all_variables(self):
         """
@@ -87,6 +183,7 @@ class storedtree(object):
     def deactivate_variable(self,varname):
         """
         """
+        # Put a print here!!!! WARNING
         if varname in self._wise_values.keys():
             raise AttributeError("Trying to de-activate a previously activated"\
                     " variable: '%s'" % varname)
@@ -104,43 +201,70 @@ class storedtree(object):
                 deactivated.append(tbranch.GetName())
         # Print deactivated info if we want...a
 
-    def book_histos(self,hc,histoVarMap):
-        """
-        Parameters
-        ----------
-        hc: PyAnUtils.HistoContainer
-            the container of histograms already initialized and ready to
-            be filled
-        histoVarMap: dict( (str,str) )
-            the link between the histogram names and the variable names.
-            Every histogram name (key of the dict) is pointing to the 
-            variable (or list of variables) which should be filled every
-            call to the 'fill' method
+    #def book_histo(self,histo,*variablenames):
+    #    """Associate an histogram (ROOT.THXF) to a list of 
+    #    variables. Each histogream is going to be filled when
+    #    the method fill_all_histos is called. Note that more
+    #    complicated syntax is allowed with the optional parameter 
+    #    `method`. 
 
-        Note
-        ----
-        The name of the histograms should match the name of the 
-        activated variables because this is the way they are going
-        to be filled
-        """
-        # Perform some consistency checks
-        for hname, variables in histoVarMap.iteritems():
-            hc.checkhisto(hname)
-            # Checking variables exist
-            if type(variables) is str:
-                if variables not in self._wise_values.keys():
-                    raise AttributeError("Not variable named '%s' has been"\
-                            " activated" % variables)
-            elif type(variables) is tuple:
-                for var in variables:
-                    if var not in self._wise_values.keys():
-                        raise AttributeError("Not variable named '%s' has been"\
-                                " activated" % var)
-            else:
-                raise RuntimeError("Not valid type for '%s'. Only str or tuple(str)"\
-                        " are accepted" % variables)
+    #    Parameters
+    #    ----------
+    #    histo: ROOT.THXF
+    #        the histogram (X=1,2,3)
+    #    variablenames: (str[,str,..])
+    #        the link between the histogram and the variables of this
+    #        ntuple. The variable should be of the type varname_method 
+    #        (see method `activate_variable`)
 
-            self._histos[variables] = getattr(hc,hname)
+    #    """
+    #    # First check the existence of the variables
+    #    listofgetters = []
+    #    for varname in variablenames:
+    #        if not hasattr(self,varname):
+    #            raise AttributeError("Variable '%s' not activated" % varname)
+    #        listofgetters.append(getattr(self,varname))
+
+    #    self._histos.append( (histo,listofgetters) )
+
+
+    #def book_histos(self,hc,histoVarMap):
+    #    """
+    #    Parameters
+    #    ----------
+    #    hc: PyAnUtils.HistoContainer
+    #        the container of histograms already initialized and ready to
+    #        be filled
+    #    histoVarMap: dict( (str,str) )
+    #        the link between the histogram names and the variable names.
+    #        Every histogram name (key of the dict) is pointing to the 
+    #        variable (or list of variables) which should be filled every
+    #        call to the 'fill' method
+
+    #    Note
+    #    ----
+    #    The name of the histograms should match the name of the 
+    #    activated variables because this is the way they are going
+    #    to be filled
+    #    """
+    #    # Perform some consistency checks
+    #    for hname, variables in histoVarMap.iteritems():
+    #        hc.checkhisto(hname)
+    #        # Checking variables exist
+    #        if type(variables) is str:
+    #            if variables not in self._wise_values.keys():
+    #                raise AttributeError("Not variable named '%s' has been"\
+    #                        " activated" % variables)
+    #        elif type(variables) is tuple:
+    #            for var in variables:
+    #                if var not in self._wise_values.keys():
+    #                    raise AttributeError("Not variable named '%s' has been"\
+    #                            " activated" % var)
+    #        else:
+    #            raise RuntimeError("Not valid type for '%s'. Only str or tuple(str)"\
+    #                    " are accepted" % variables)
+
+    #        self._histos[variables] = getattr(hc,hname)
     
     def getentries(self):
         """
@@ -150,26 +274,33 @@ class storedtree(object):
     def getentry(self,i):
         """
         """
+        if self._currententry == i:
+            return
         _dummy = self._tree.GetEntry(i)
+        self._currententry = i
 
-    def fill(self,i):
-        """
-        """
-        self.getentry(i)
-        for variables,histo in self._histos.iteritems():
-            if type(variables) is str:
-                histo.Fill( self._wise_values[variables] )
-            elif type(variables) is tuple:
-                x,y = variables[0],variables[1]
-                if len(variables) == 3:
-                    z = variables[2]
-                    histos.Fill(x,y,z)
-                else:
-                    histos.Fill(x,y)
+    #def fill_all_histos(self):
+    #    """
+    #    """
+    #    pass
+
+    #def fill(self,i):
+    #    """
+    #    """
+    #    self.getentry(i)
+    #    for histo,m in self._histos.iteritems():
+    #        x = m()
+    #        if len(m) >= 3:
+    #            x = variables[0],variables[1]
+    #            if len(variables) == 3:
+    #                z = variables[2]
+    #                histos.Fill(x,y,z)
+    #            else:
+    #                histos.Fill(x,y)
 
 
 
-class xaod(storedtree):
+class xaodtree(storedtree):
     """.. class rpvmcinfo(rootfiles)
     concrete storedeff class for efficiencies evaluated using the 
     RPVMCInfoTree ATLAS package 
@@ -185,6 +316,16 @@ class xaod(storedtree):
         import PyCintex
         PyCintex.Cintex.Enable()
         # Not use this, just look cyypy, and the other stuff..
-        from AthenaROOTAccess import transientTree  
+        from AthenaROOTAccess import transientTree
 
-        super(xaod,self).__init__(rootfiles,'CollectionTree')
+        super(xaodtree,self).__init__(rootfiles,'CollectionTree')
+
+        # re-do the tree, to associate properly auxliar data and so on
+        # FIXME:: VERY PROVISIONAL FIXME::
+        self._file = ROOT.TFile(rootfiles)
+        del self._tree
+        self._tree = transientTree.makeTree(self._file)
+
+        # Initialize the tree, just to be able to set memory addresses
+        self.getentry(0)
+
